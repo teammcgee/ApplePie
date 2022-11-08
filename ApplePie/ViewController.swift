edd132576b91d615f6525f7830fdc45f62daa374
Aidan McGee <aidanmcgee906>
2022-11-08 12:47:33 -0500

    ApplePie

--- ApplePie/ViewController.swift
+++ ApplePie/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // MARK: - IB Outlets
+    @IBOutlet var treeImageView: UIView!
+    @IBOutlet weak var wordLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet var letterButtons: [UIButton]!
+    
+    // MARK: - Instance Properties
+    
+    
+   
+    //MARK: - VDL
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    // MARK: - IB Actions
+    @IBAction func letterButtonTapped(_ sender: UIButton) {
+        sender.isEnabled = false
+    }
+    
+    
+    // MARK: -
 
 }
 
